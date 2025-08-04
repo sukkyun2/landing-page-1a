475c3f6363b45ede6f41b14f390e6789fc162602
sukkyun hong <38815540+sukkyun2@users.noreply.github.com>
2025-08-04 23:08:26 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,31 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from 'next';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: '시점',
   description: '시점: 세상을 보는 가장 빠른 시점',
   generator: 'RedStone',
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <head>
-        <style>{
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
-        }</style>
+        <style>{`
+          html {
+            font-family: ${GeistSans.style.fontFamily};
+            --font-sans: ${GeistSans.variable};
+            --font-mono: ${GeistMono.variable};
+          }
+        `}</style>
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
