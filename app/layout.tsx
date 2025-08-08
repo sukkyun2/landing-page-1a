efa070b0ca659f44ddd93f62289ff51885a59cfc
sukkyun2 <goobghd@gmail.com>
2025-08-08 15:54:43 +0900

    chore: 뷰저블 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,24 @@ export default function RootLayout({
             --font-mono: ${GeistMono.variable};
           }
                   `}</style>
+
+          {/* Beusable RUM Script */}
+          <Script id="beusable-rum" strategy="afterInteractive">
+              {`
+            (function(w, d, a){
+              w.__beusablerumclient__ = {
+                load: function(src){
+                  var b = d.createElement("script");
+                  b.src = src;
+                  b.async = true;
+                  b.type = "text/javascript";
+                  d.getElementsByTagName("head")[0].appendChild(b);
+                }
+              };
+              w.__beusablerumclient__.load(a + "?url=" + encodeURIComponent(d.URL));
+            })(window, document, "//rum.beusable.net/load/b250618e175014u887");
+          `}
+          </Script>
       </head>
       <body>
         {children}
