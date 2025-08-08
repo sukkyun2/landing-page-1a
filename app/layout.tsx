3c4efb824b9d68e3ad06cb07368f84c6bd74cdcd
sukkyun hong <38815540+sukkyun2@users.noreply.github.com>
2025-08-08 14:48:03 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
+// app/layout.tsx or app/RootLayout.tsx
+
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
+import Script from 'next/script'
 
-export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+export const meta Metadata = {
+  title: '시점',
+  description: '시점: 세상을 보는 가장 빠른 시점',
+  generator: 'RedStone',
 }
 
 export default function RootLayout({
@@ -15,17 +18,44 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <head>
-        <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
-        `}</style>
+        {/* Facebook Pixel Script */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1021764120037115');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* Font configuration for Tailwind */}
+                  <style>{`
+          html {
+            font-family: ${GeistSans.style.fontFamily};
+            --font-sans: ${GeistSans.variable};
+            --font-mono: ${GeistMono.variable};
+          }
+                  `}</style>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        {/* Facebook NoScript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1021764120037115&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </body>
     </html>
   )
 }
